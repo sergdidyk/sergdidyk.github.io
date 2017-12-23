3d1217370c9b7debc6fa55c100328030c71d0175
Serhiy Didyk <serg.didyk@gmail.com>
2017-12-23 21:59:10 +0200

    forms validated

--- js/common.js
+++ js/common.js
@@ -145,26 +145,120 @@ $(function(){
     });
 	});
 
-	/* Клонировать инфо (фото, цену) в форму и удалять по закрытию*/
+	/* -------- FORMS VALIDATION --------- */
+	jQuery.validator.addMethod("phonenumber", function(phone_number, element) {
+	    phone_number = phone_number.replace(/\s+/g, "");
+	    return this.optional(element) || phone_number.length > 9 && 
+	    phone_number.match(/^((\+[1-9]{1,4}[ \-]*)|(\([0-9]{2,3}\)[ \-]*)|([0-9]{2,4})[ \-]*)*?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/);
+	}, "<br />Будь-ласка, вкажіть вірний номер телефону");
+
+	$(".callback_form").validate({
+		rules: {
+			callback_name: {
+				required: true,
+				maxlength: 20
+			}, 
+			callback_phone: {
+				required: true,
+				phonenumber: true
+			}
+		}, 
+		messages: {
+			callback_name: {
+				required: "Будь-ласка, введіть своє ім'я",
+				maxlength: "Максимальна кількість символів: 30"
+			}, 
+			callback_phone: {
+				required: "Введіть свій телефон, щоб ми могли зателефонувати Вам"
+			}
+		}
+	});
+
+	$(".order_form").validate({
+		rules: {
+			order_name: {
+				required: true,
+				maxlength: 30
+			}, 
+			order_phone: {
+				required: true,
+				phonenumber: true
+			}, 
+			order_email: {
+				required: true,
+				email: true
+			},
+			order_comment: {
+				maxlength: 200
+			}
+
+		}, 
+		messages: {
+			order_name: {
+				required: "Будь-ласка, введіть своє ім'я",
+				maxlength: "Максимальна кількість символів: 30"
+			}, 
+			order_phone: {
+				required: "Введіть свій телефон, щоб ми могли зателефонувати Вам"
+			}, 
+			order_email: {
+				required: "Будь-ласка, введіть свій email",
+				email: "Введіть вірний формат email адреси"
+			},
+			order_comment: {
+				maxlength: "Максимальна кількість символів: 200"
+			}
+		}
+	});
+
 	$(".buy_button").click(function(){
 		var x = $(this).parent().parent(); // родительский элемент 2-го уровня
-		var main_image = x.find(".main_photo"); // ищем основное фото товара
-		var good_price = x.find(".prod_currentprice>span, .prod_newprice>span"); //	ищем цену товара
-		main_image.clone().appendTo(".modal_good_photo").addClass("img-thumbnail"); //клонируем фото в форму		
-		good_price.clone().appendTo(".modal_good_price h2").addClass("form_price"); //клонируем цену в форму	
+		var prod_name = x.find("div.prod_name").html(); 
+		var prod_descr = x.find("div.prod_descr").html();
+		var input_value = $("#order_product").val(prod_name); // назв. товара в input
+		var input_value = $("#order_product_descr").val(prod_descr);
+
+		var good_price = x.find(".prod_currentprice>span, .prod_newprice>span").html(); //	ищем цену товара
+		$("#order_price").val(good_price);
 	});
 
-	$(".close_form, .send_form, .close").click(function(){
-			setTimeout(function(){
-				$(".modal_good_photo .main_photo").remove(); // удаление на setTimeout, чтобы фото удалялось уже после полного закрытия модального окна
-				$(".modal_good_price h2>span").remove(); // удаляем цену из формы
-			}, 500);
+	$(".customorder_form").validate({
+		rules: {
+			custom_name: {
+				required: true,
+				maxlength: 20
+			}, 
+			custom_phone: {
+				required: true,
+				phonenumber: true
+			},
+			custom_email: {
+				required: true,
+				email: true
+			},
+			custom_comment: {
+				required: true,
+				maxlength: 300
+			}
+		}, 
+		messages: {
+			custom_name: {
+				required: "Будь-ласка, введіть своє ім'я",
+				maxlength: "Максимальна кількість символів: 30"
+			}, 
+			custom_phone: {
+				required: "Введіть свій телефон, щоб ми могли зателефонувати Вам"
+			},
+			custom_email: {
+				required: "Будь-ласка, введіть свій email",
+				email: "Введіть вірний формат email адреси"
+			},
+			custom_comment: {
+				required: "Нам потрібен короткий опис Вашої ідеї",
+				maxlength: "Максимальна кількість символів: 300"
+			}
+		}
 	});
-
-	/* -------- FORMS VALIDATION --------- */
-	
-	
-
 
 	/* --------END OF FORMS VALIDATION ---------- */
 
