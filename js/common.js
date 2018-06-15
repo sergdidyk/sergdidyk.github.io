d64854ead9d171d83a51cbf2a0dd29e5e11be603
sergdidyk <serg.didyk@gmail.com>
2018-06-15 18:23:17 +0300

    test

---
+++ js/common.js
@@ -0,0 +1,34 @@
+$(function(){
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); }); //Prevent dragging images
+
+	//Hamburger menu animation
+		$("#nav-icon3").click(function(){
+			$(this).toggleClass("open");
+		});
+
+	//Mobile menu toggle	
+	let mainNav = document.getElementById('main-nav');
+	let navbarToggle = document.getElementById('ham');
+
+	navbarToggle.addEventListener('click',function(){
+	    if(this.classList.contains('active')){
+	    		this.classList.remove('active');
+	    		$("#main-nav").slideUp();
+	    	
+	    }else{
+	    		this.classList.add('active');
+	    		$("#main-nav").slideDown();
+	    }
+	});
+
+});
